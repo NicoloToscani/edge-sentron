07e48c8d6e3b603f5ed3ec4655e2bd2dc964eeaf
Nicolò Toscani <toscani.nicolo90@gmail.com>
2021-11-26 16:21:26 +0100

    Add files via upload

--- named pipe example/p2.py
+++ named pipe example/p2.py
@@ -2,8 +2,10 @@ import os
 import errno
 import time
 
+import sys
+
 file_name = "/tmp/HmiRuntime"
-mode = 0o600 # RW permisison
+mode = 0o777 # RW permisison
 pipe = 0 
        
         
@@ -17,7 +19,24 @@ except OSError as oe:
        print("File already exist")
 
 while(True):       
-     f = open(file_name, 'r')
-     read_value = f.read();
+     f = os.open(file_name, os.O_RDWR|os.O_CREAT)
+     len_bytes = sys.getsizeof(f)
+     print(len_bytes)
+     read_value = os.read(f, len_bytes) 
+     app = read_value.decode('UTF-8')
      print(read_value)
-         
+     os.close(pipe)
+     
+     '''
+     # Scrivo per leggere altro lato
+     print("Call write and wait")
+     pipe = os.open(file_name, os.O_RDWR|os.O_CREAT)
+     print("Scrivo")
+     write_value = ('Ti sto scrivendo dal processo 2')
+     s = write_value.encode('UTF-8')
+     print(s)
+     os.write(pipe,s)
+     os.close(pipe)
+     '''
+     time.sleep(8)
+     
